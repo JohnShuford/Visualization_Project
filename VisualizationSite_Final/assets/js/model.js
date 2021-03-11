fc12891d241d6a6fc1e03da5fb7db1680bebad1c
jeosqueri <juliasqueri303@gmail.com>
2021-03-10 19:49:19 -0700

    added to model.js, model_get.html

--- VisualizationSite_Final/assets/js/model.js
+++ VisualizationSite_Final/assets/js/model.js
@@ -1,7 +1,15 @@
-var button = d3.select("#title");
+var button = d3.select("#submit");
+var url = "http://127.0.0.1:5000/classifier?title="
+var form = d3.select("#form");
+
 
 button.on("click", function() {
-    d3.request(url)
+    var title = d3.select("#title");
+    var titleValue = title.property("value");
+    
+    var category = d3.request(url + titleValue)
         .mimeType("application/json")
         .response(function(xhr) { return JSON.parse(xhr.responseText); });
+    
+    console.log(category);
 });
