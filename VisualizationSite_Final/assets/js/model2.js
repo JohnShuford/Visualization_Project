89a0e455452c7832efcb414340da3027ee196680
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-10 22:02:55 -0700

    model2 js

--- VisualizationSite_Final/assets/js/model2.js
+++ VisualizationSite_Final/assets/js/model2.js
@@ -1,17 +1,24 @@
 var button = d3.select("#submit");
 var url = "http://127.0.0.1:5000/api/v1.0/classifier/"
 var form = d3.select("#form");
-
+console.log(url);
 
 button.on("click", function() {
     var title = d3.select("#title");
     var titleValue = title.property("value");
 
-    d3.json(url + titleValue, function(error, data){
-        console.log(error);
-        console.log(data);
+    console.log(titleValue);
+
+    d3.json(url + titleValue, function(data){
+        d3.select("#pid").text(data.category)
+         console.log(data);
+         console.log("We are here");
     })
     
+//     d3.json("https://api.npoint.io/5b22a0474c99d3049d2e", function(data) {
+//     console.log(data)
+// })
+
     // var category = d3.request(url + titleValue)
     //     .mimeType("application/json")
     //     .response(function(xhr) {parsed = JSON.parse(xhr.responseText); 
